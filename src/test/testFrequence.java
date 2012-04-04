e477bbad7b7c0ff6b974d5323f19a16ac0f96b54
Hersand Maxime <mhersand@epsi.fr>
2012-04-04 22:55:35 +0200

    debut du projet

---
+++ src/test/testFrequence.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testFrequence {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
