5bf07e13ae65f4d8e5b69e4dd0efafad1414437f
bteyssier <bteyssier@epsi.fr>
2012-05-01 15:24:55 +0200

    rajout des Jtextfield

--- src/fourier/Affichage.java
+++ src/fourier/Affichage.java
@@ -1,17 +1,41 @@
 package fourier;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 public class Affichage {
 
-	private JFrame fenetre;
-	private JLabel affichage; 
+	
+	private JFrame fenetre = new JFrame("Projet Math");
+	private JPanel container = new JPanel();
+	private JLabel affichage = new JLabel("test");
+	private JTextField champA = new JTextField(0); 
+    private JTextField champB = new JTextField(0); 
+	
 	
 	public Affichage() { 
 		
-        this.fenetre = new JFrame("Projet Math"); 
-        this.fenetre.setVisible(true); 
-        this.affichage = new JLabel();
+		
+        fenetre.setSize(1024,500);
+        fenetre.setLocationRelativeTo(null);
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        
+        container.setBackground(Color.white);
+        container.setLayout(new BorderLayout());
+        
+        JPanel top = new JPanel();
+        
+        champA.setPreferredSize(new Dimension(50, 30));
+        champB.setPreferredSize(new Dimension(50, 30));
+        
+        top.add(affichage);
+        top.add(champA);
+        top.add(champB);
+        
+        container.add(top, BorderLayout.NORTH);
+        fenetre.setContentPane(container);
+        fenetre.setVisible(true);
     }
 }
