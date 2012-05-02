ebec34d33c14f2f5fb1073822e49aa79fe88a2fd
Hersand Maxime <mhersand@epsi.fr>
2012-05-02 11:55:54 +0200

    son presque parfait

--- src/fourier/SerieDeFourier.java
+++ src/fourier/SerieDeFourier.java
@@ -12,7 +12,7 @@ public class SerieDeFourier {
 	public double getValeur(double t){
 		double sf = 0;
 		for(int i = 0; i < spectre.getlength(); i++)
-			sf += Math.cos(2*Math.PI * spectre.getFrequence(i) * t);
+			sf += spectre.getFrequence(i) * Math.cos(2*Math.PI * Math.pow(2, i+5) * t);
 		
 		return sf;
 	}
